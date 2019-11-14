6fbc8e385b1c21e92d0f411dcdda19d66f87d1dd
facutorraca <ftorraca@fi.uba.ar>
2019-11-14 19:27:39 -0300

    agrego unos ejemplitos de ownership

---
+++ ejemplos/Ownership/ownership_1.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+
+/*--------------------------------------------------*/
+/* C++ al realizar un move no elimina al objeto del */
+/* scope en que esta, este sigue existiendo         */
+/* con sus elementos alterados                      */
+/*--------------------------------------------------*/
+
+class Numero {
+
+    int* numero;
+
+    public:
+        Numero(int numero) {
+            this->numero = new int(numero);
+        }
+
+        Numero(Numero&& otro) {
+            this->numero = otro.numero;
+            //otro.numero = nullptr;
+        }
+
+        void imprimir() {
+            std::cout << "EL numero es: " << *this->numero << std::endl;
+        }
+
+        ~Numero() {
+            delete this->numero;
+        }
+};
+
+void funcion_que_hace_algo_con_string(std::string string) {
+    string.append("!!!");
+    std::cout << "El string movido: " << string << std::endl;
+}
+
+void funcion_que_hace_algo_con_vector(std::vector<int*> vector_de_punteros) {
+    vector_de_punteros.push_back(new int(*vector_de_punteros[0] + 2));
+    std::cout << "Elem 0 del vector movido: " << *vector_de_punteros[0] << std::endl;
+    std::cout << "Elem 1 cel vector movido: " << *vector_de_punteros[1] << std::endl;
+}
+
+void funcion_que_hace_algo_con_numero(Numero numero) {
+    numero.imprimir();
+}
+
+int main() {
+    std::string string("Hola, soy un string");
+    funcion_que_hace_algo_con_string(std::move(string));
+    std::cout << "Quedo algo? " << string << std::endl;
+
+    std::vector<int *> vector_de_punteros;
+    vector_de_punteros.push_back(new int(1));
+    funcion_que_hace_algo_con_vector(std::move(vector_de_punteros));
+    std::cout << "Quedo algo?" << *vector_de_punteros[0] << std::endl;
+
+    Numero numero(5);
+    funcion_que_hace_algo_con_numero(std::move(numero));
+    numero.imprimir();
+
+    return 0;
+}
---
+++ ejemplos/Ownership/ownership_1.rs
@@ -0,0 +1,57 @@
+use std::io;
+
+/*--------------------------------------*/
+/*Rust pasa todas las cosas por defecto */
+/*como movimiento, no dejandote acceder */
+/*despues de moverlas en tiempo de      */
+/*compilacion                           */
+/*--------------------------------------*/
+
+struct Numero {
+    valor : Box<i32>
+}
+
+impl Numero {
+
+    pub fn new(numero : i32) -> Numero {
+        Numero{ valor: Box::new(numero) }
+    }
+
+    pub fn imprimir(&self) {
+        println!("El numero es: {}", &self.valor);
+    }
+}
+
+
+fn funcion_que_hace_algo_con_string(mut string: String) {
+    string.push('!');
+    println!("{}", string);
+}
+
+fn funcion_que_hace_algo_con_vector(mut vector: Vec<Box<i32>>) {
+    vector.push(Box::new(3));
+    println!("{:?}", vector);
+}
+
+fn funcion_que_hace_algo_con_numero(numero: Numero) {
+    numero.imprimir();
+    //numero.imprimir();
+}
+
+fn main() -> io::Result<()> {
+    let string = String::from("Hola, soy un string");
+    funcion_que_hace_algo_con_string(string);
+    //println!("{}", string);
+
+    let mut vector : Vec<Box<i32>> = Vec::new();
+    vector.push(Box::new(1));
+    vector.push(Box::new(2));
+    funcion_que_hace_algo_con_vector(vector);
+    //println!("{:?}", vector);
+
+    let numero = Numero::new(1);
+    funcion_que_hace_algo_con_numero(numero);
+    //numero.imprimir();
+
+    return Ok(());
+}
